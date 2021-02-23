33d8c883ad1f960ceb6eb054259d55f1e46793dc
Rodrigo de França <howdrigo.fr@gmail.com>
2021-02-23 01:06:30 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ExperienceBar from './components/ExperienceBar';
+import './styles/global.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
